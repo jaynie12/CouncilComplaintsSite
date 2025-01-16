0d8424126851da20a4c1d07bc6c5e8c950b5fdb7
jaynie12 <jaynieshah2000@gmail.com>
2025-01-16 20:43:08 +0000

    commt intial front end

---
+++ CouncilComplaintsSite/frontEnd/CouncilComplaintsApp/src/App.tsx
@@ -0,0 +1,8 @@
+import MessageTest from "./test";
+
+
+function App(){
+return <div><MessageTest></MessageTest></div>
+}
+
+export default App;
---
+++ CouncilComplaintsSite/frontEnd/CouncilComplaintsApp/src/test.tsx
@@ -0,0 +1,6 @@
+function MessageTest(){
+    //returning a result of a function
+    return <h1>Hello World{'Jaynie'}</h1>
+}
+
+export default MessageTest;
